4337c26526857f6835ff8fcfb53a1c48e2a76022
azis376889-hue <azis376889@gmail.com>
2026-04-17 12:09:21 +0700

    Update project-setup.bash

--- project-setup.bash
+++ project-setup.bash
@@ -1,3 +1,5 @@
 npx create-next-app@latest animestream --typescript --tailwind --eslint --app --src-dir --import-alias "@/*"
 cd animestream
 npm install lucide-react @types/node
+npm install axios swr
+npm install -D @types/node
